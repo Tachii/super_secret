3518f45351792bc8947760942b3cfb341b02e9ef
Gleb Zaveruha <zaveruha007@gmail.com>
2016-09-03 18:05:15 +0300

    connected database and ran migrations, authorization is working (#2)

--- config/database.php
+++ config/database.php
@@ -54,16 +54,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'us-cdbr-iron-east-04.cleardb.net',
+            'database' => 'heroku_836985ab75e6476',
+            'username' => 'bf936efabbf2be',
+            'password' => '155219aa',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => false,
-            'engine' => null,
         ],
 
         'pgsql' => [
